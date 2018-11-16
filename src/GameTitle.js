778c134b5c68ae9a0446d7c953841ff87afbd675
Shin Changseon <skywing27@gmail.com>
2018-11-16 19:00:38 +0900

    edit gameTitle

--- src/GameTitle.js
+++ src/GameTitle.js
@@ -4,7 +4,7 @@ import './css/GameTitle.scss'
 const GameTitle = (prop) => {
   return (
     <div className="gameTitle">
-      <h2>Girl Gruop WorldCup Round of {prop.round} : {prop.sequence}/{prop.round / 2}</h2>
+      <h2>Girl Group WorldCup Round of {prop.round} : {prop.sequence}/{prop.round / 2}</h2>
     </div>
   );
 };
