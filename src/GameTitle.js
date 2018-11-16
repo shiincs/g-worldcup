70f4cc6812218d172da8f6d14f03f2c647523873
hellomac87 <hellomac87@gmail.com>
2018-11-16 12:08:58 +0900

    Style: upgrade css

--- src/GameTitle.js
+++ src/GameTitle.js
@@ -4,7 +4,7 @@ import './css/GameTitle.scss'
 const GameTitle = () => {
   return (
     <div className="gameTitle">
-      <h2>여자 아이돌 월드컵 16강 1/8</h2>
+      <h2>Girl Gruop WorldCup Round of 16 : 1/8</h2>
     </div>
   );
 };
